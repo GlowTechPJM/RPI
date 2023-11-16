c80c5c1d9efb278817a5c89772f8d224c70ce63c
ManelRuano <ruano.garcia.manel@gmail.com>
2023-11-16 15:50:08 +0100

    scroll

--- server_java/src/main/java/com/project/ChatServer.java
+++ server_java/src/main/java/com/project/ChatServer.java
@@ -236,7 +236,7 @@ public class ChatServer extends WebSocketServer {
 
     public static void executeDisplayCommand(String text) {
         try {
-            String command = "cd ~/dev/rpi-rgb-led-matrix && examples-api-use/text-scroll -x 5 -y 18 -f ~/dev/bitmap-fonts/bitmap/cherry/cherry-10-b.bdf --led-cols=64 --led-rows=64 --led-slowdown-gpio=4 --led-no-hardware-pulse";
+            String command = "cd ~/dev/rpi-rgb-led-matrix && ./text-scroller -f ~/dev/bitmap-fonts/bitmap/cherry/cherry-10-b.bdf --led-cols=64 --led-rows=64 --led-slowdown-gpio=4 --led-no-hardware-pulse \""+text;
             ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
             Process proceso = processBuilder.start();
 
